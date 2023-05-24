ca0a6f75e4c48b24ed1f9c9268610df10e5d9ada
Santiago <64978794+dmirandam@users.noreply.github.com>
2023-05-24 16:23:33 -0500

    Update img.tf

--- img.tf
+++ img.tf
@@ -3,10 +3,10 @@ variable "user_d" {
   description = "User data script for EC2 instance"
   default = <<-EOT
               #!/bin/bash
-              apt-get update
-              apt-get install -y docker.io
-              docker pull maherreramu/ag-front
-              docker run -d -p 80:80 maherreramu/ag-front
+              sudo apt-get update
+              sudo apt-get install -y docker.io
+              sudo docker pull maherreramu/ag-front
+              sudo docker run -d -p 80:80 maherreramu/ag-front
               EOT
 }
 
@@ -17,7 +17,7 @@ resource "aws_instance" "aaaaaaa" {
   user_data               = var.user_d
   vpc_security_group_ids = ["sg-092ecf1ac36a65467"]
   tags = {
-    Name = "is2"
+    Name = "yara_care_pipi"
   }
   
 }
